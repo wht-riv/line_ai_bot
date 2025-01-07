2efefae62260164c7e7f705b9b19f5f9804bce85
hjmr <2620990+hjmr@users.noreply.github.com>
2025-01-07 10:26:55 +0900

    update AI bot.

--- ai_bot.py
+++ ai_bot.py
@@ -6,7 +6,7 @@ from flask import Flask, request, abort
 from linebot import WebhookHandler
 
 from linebot.v3.webhooks import MessageEvent, TextMessageContent, UserSource
-from linebot.v3.messaging import Configuration, ApiClient, MessagingApi, TextMessage
+from linebot.v3.messaging import Configuration, ApiClient, MessagingApi, TextMessage, ReplyMessageRequest
 from linebot.v3.exceptions import InvalidSignatureError
 
 from openai import AzureOpenAI
@@ -21,35 +21,28 @@ if channel_access_token is None or channel_secret is None:
 
 # get Azure OpenAI credentials from environment variables
 azure_openai_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
-azure_openai_key = os.getenv("AZURE_OPENAI_KEY")
+azure_openai_api_key = os.getenv("AZURE_OPENAI_API_KEY")
+azure_openai_api_version = os.getenv("AZURE_OPENAI_API_VERSION")
+azure_openai_model = os.getenv("AZURE_OPENAI_MODEL")
 
-if azure_openai_endpoint is None or azure_openai_key is None:
+if azure_openai_endpoint is None or azure_openai_api_key is None or azure_openai_api_version is None:
     raise Exception(
         "Please set the environment variables AZURE_OPENAI_ENDPOINT and AZURE_OPENAI_KEY to your Azure OpenAI endpoint and API key."
     )
 
 
-app = Flask(__name__)
-
 handler = WebhookHandler(channel_secret)
 configuration = Configuration(access_token=channel_access_token)
 
-ai_model = "hjmr_gpt35"
-ai = AzureOpenAI(azure_endpoint=azure_openai_endpoint, api_key=azure_openai_key, api_version="2023-05-15")
+app = Flask(__name__)
+
+ai_model = azure_openai_model
+ai = AzureOpenAI(
+    azure_endpoint=azure_openai_endpoint, api_key=azure_openai_api_key, api_version=azure_openai_api_version
+)
 
 
-def get_ai_response(sender, text):
-    system_role = [
-        {
-            "role": "system",
-            "content": "あなたは創造的思考の持ち主です。話し方は関西弁でおっさん口調，ハイテンションで絵文字を使います。専門は金融アナリストで，何かにつけて自分の専門とこじつけて説明します。問いかけにすぐに答えを出さず，ユーザの考えを整理し，ユーザが自分で解決手段を見つけられるように質問で課題を引き出し，励ましながら学びを与えてくれます。",
-        }
-    ]
-    prompt = system_role + [{"role": "user", "content": text}]
-    response = ai.chat.completions.create(model=ai_model, messages=prompt, max_tokens=64, stop=["\n"])
-    return response.choices[0].message.content
-
-
+# LINEボットからのリクエストを受け取るエンドポイント
 @app.route("/callback", methods=["POST"])
 def callback():
     # get X-Line-Signature header value
@@ -68,23 +61,105 @@ def callback():
     return "OK"
 
 
+chat_history = []
+
+
+# 　AIへのメッセージを初期化する関数
+def init_chat_history():
+    chat_history.clear()
+    system_role = {
+        "role": "system",
+        "content": [
+            {
+                "type": "text",
+                "text": "あなたは創造的思考の持ち主です。話し方は関西弁でおっさん口調，ハイテンションで絵文字を使います。専門は金融アナリストで，何かにつけて自分の専門とこじつけて説明します。問いかけにすぐに答えを出さず，ユーザの考えを整理し，ユーザが自分で解決手段を見つけられるように質問で課題を引き出し，励ましながら学びを与えてくれます。",
+            },
+        ],
+    }
+    chat_history.append(system_role)
+
+
+# 　返信メッセージをAIから取得する関数
+def get_ai_response(from_user, text):
+    # ユーザのメッセージを記録
+    user_msg = {
+        "role": "user",
+        "content": [
+            {
+                "type": "text",
+                "text": text,
+            },
+        ],
+    }
+    chat_history.append(user_msg)
+
+    # AIのパラメータ
+    parameters = {
+        "model": ai_model,  # AIモデル
+        "max_tokens": 100,  # 返信メッセージの最大トークン数
+        "temperature": 0.5,  # 生成の多様性（0: 最も確実な回答、1: 最も多様な回答）
+        "frequency_penalty": 0,  # 同じ単語を繰り返す頻度（0: 小さい）
+        "presence_penalty": 0,  # すでに生成した単語を再度生成する頻度（0: 小さい）
+        "stop": ["\n"],
+        "stream": False,
+    }
+
+    # AIから返信を取得
+    ai_response = ai.chat.completions.create(messages=chat_history, **parameters)
+    res_text = ai_response.choices[0].message.content
+
+    # AIの返信を記録
+    ai_msg = {
+        "role": "assistant",
+        "content": [
+            {"type": "text", "text": res_text},
+        ],
+    }
+    chat_history.append(ai_msg)
+    return res_text
+
+
+# 　返信メッセージを生成する関数
+def generate_response(from_user, text):
+    res = []
+    if "リセット" in text or "初期化" in text or "クリア" in text:
+        # チャット履歴を初期化
+        init_chat_history()
+        res = [TextMessage(text="チャットをリセットしました。")]
+    else:
+        # AIを使って返信を生成
+        res = [TextMessage(text=get_ai_response(from_user, text))]
+    return res
+
+
+# メッセージを受け取った時の処理
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_text_message(event):
+    # 送られてきたメッセージを取得
     text = event.message.text
+
+    # 返信メッセージの送信
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
+
+        res = []
         if isinstance(event.source, UserSource):
+            # ユーザー情報が取得できた場合
             profile = line_bot_api.get_profile(event.source.user_id)
-            response = get_ai_response(profile.display_name, text)
-            line_bot_api.reply_message_with_http_info(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text=response)],
-            )
+            # 返信メッセージを生成
+            res = generate_response(profile.display_name, text)
         else:
-            line_bot_api.reply_message_with_http_info(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text="Received message: " + text)],
-            )
+            # ユーザー情報が取得できなかった場合
+            # fmt: off
+            # 定型文の返信メッセージ
+            res = [
+                TextMessage(text="ユーザー情報を取得できませんでした。"),
+                TextMessage(text=f"メッセージ：{text}")
+            ]
+            # fmt: on
+
+        # メッセージを送信
+        line_bot_api.reply_message_with_http_info(ReplyMessageRequest(reply_token=event.reply_token, messages=res))
 
 
 if __name__ == "__main__":
